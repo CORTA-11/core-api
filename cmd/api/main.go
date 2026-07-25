68d6573acfc3a1244fa783a71abc34f2a38a409d
Teshan Kannangara <tpk.teshan@gmail.com>
2026-07-25 13:16:48 +0530

    go project initialization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+
+	r := http.NewServeMux()
+
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := w.Write([]byte("Hello World")); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	s := http.Server{
+		WriteTimeout: time.Second * 5,
+		ReadTimeout:  time.Second * 5,
+		Addr:         ":8080",
+		Handler:      r,
+	}
+
+	log.Fatal(s.ListenAndServe())
+}
