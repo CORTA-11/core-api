44ec8a7d2082dea053a1beb8eeab1f65b4342abe
Teshan Kannangara <tpk.teshan@gmail.com>
2026-07-27 19:30:32 +0530

    replace net/http with chi

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,6 +11,9 @@ import (
 
 	"github.com/CORTA-11/core-api/internal/repository"
 	"github.com/jackc/pgx/v5"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
@@ -30,16 +33,16 @@ func main() {
 
 	queries := repository.New(conn)
 
-	r := http.NewServeMux()
-
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
 	// TODO: Extract the handlers to a separate file
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		if _, err := w.Write([]byte("Hello World")); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
-	r.HandleFunc("/orgs", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/orgs", func(w http.ResponseWriter, r *http.Request) {
 		orgs, err := queries.GetOrgs(r.Context())
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
