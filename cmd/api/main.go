2a9d6471a34d467d774de1b4dda6f0179f3616da
Teshan Kannangara <tpk.teshan@gmail.com>
2026-07-27 20:27:27 +0530

    refactor handler to a new file

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"log"
 	"net/http"
@@ -11,13 +10,9 @@ import (
 
 	"github.com/CORTA-11/core-api/internal/repository"
 	"github.com/jackc/pgx/v5"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-
 	ctx := context.Background()
 
 	// TODO: Use pgxpool
@@ -33,36 +28,14 @@ func main() {
 
 	queries := repository.New(conn)
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	// TODO: Extract the handlers to a separate file
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		if _, err := w.Write([]byte("Hello World")); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
-	r.Get("/orgs", func(w http.ResponseWriter, r *http.Request) {
-		orgs, err := queries.GetOrgs(r.Context())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-
-		err = json.NewEncoder(w).Encode(orgs)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-	})
+	router := NewRouter(queries)
+	router.SetupRoutes()
 
 	s := http.Server{
 		WriteTimeout: time.Second * 5,
 		ReadTimeout:  time.Second * 5,
 		Addr:         ":8080",
-		Handler:      r,
+		Handler:      router.Handler(),
 	}
 
 	if err := s.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
